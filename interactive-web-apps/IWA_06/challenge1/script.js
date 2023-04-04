ed55eaed736c8b07e0fd9f3fa75ac4938824b844
Katlego Christabel Leshiba <106029638+katleschris@users.noreply.github.com>
2023-04-04 13:06:40 +0200

    Fixed the NaN issue

--- interactive-web-apps/IWA_06/challenge1/script.js
+++ interactive-web-apps/IWA_06/challenge1/script.js
@@ -2,9 +2,8 @@ const primaryPhone = 'O748105141'
 const secondaryPhone = '0219131568'
 
 // Only change below this line
-
-const primaryValid = isNaN(primaryPhone) 
-const secondaryValid = isNaN(secondaryPhone)
+const primaryValid = !isNaN(primaryPhone) && primaryPhone !== ''
+const secondaryValid = !isNaN(secondaryPhone) && secondaryPhone !== ''
 
 console.log('Primary phone is valid numerical string:', primaryValid)
 console.log('Secondary phone is valid numerical string:', secondaryValid )
