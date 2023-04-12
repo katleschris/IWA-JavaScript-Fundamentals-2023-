44adae4a07c6c86cd7fca576875e9af01e4b6809
Katlego Christabel Leshiba <106029638+katleschris@users.noreply.github.com>
2023-04-12 13:32:39 +0200

    Used correct strings and logical operators

---
+++ interactive-web-apps/IWA_07/challenge 2/script.js
@@ -0,0 +1,10 @@
+const nickname= "Timmy";
+const firstname = "Timothy";
+
+if (nickname) {
+    console.log(`Good morning, ${nickname}!`);
+    } else if (firstname) {
+    console.log(`Good morning, ${firstname}!`);
+    } else {
+    console.log("Good morning!");
+    }
