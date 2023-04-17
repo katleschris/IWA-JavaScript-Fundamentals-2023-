b61f15f5643de9df6cf47eca12441707b10e3c1f
Katlego Christabel Leshiba <106029638+katleschris@users.noreply.github.com>
2023-04-17 13:06:16 +0200

    Investigated and fixed errors

--- interactive-web-apps/IWA_11/Challenge 1/script.js
+++ interactive-web-apps/IWA_11/Challenge 1/script.js
@@ -14,24 +14,24 @@ const status2= order2.querySelector('.status dd');
 
 //For order 3
 const order3= document.querySelector('[data-key = "order3"]');
-const biscuits3= order2.querySelector('.biscuits.count');
-const donuts3= order2.querySelector('.donuts.count');
-const pancakes3= order2.querySelector('.pancakes.count');
-const status3= order2.querySelector('.status dd');
+const biscuits3= order3.querySelector('.biscuits.count');
+const donuts3= order3.querySelector('.donuts.count');
+const pancakes3= order3.querySelector('.pancakes.count');
+const status3= order3.querySelector('.status dd');
 
 // replace the loading prompts with the data from the database
 
 biscuits1.textContent= order1.dataset.biscuits;
 donuts1.textContent = order1.dataset.donuts;
-pancakes1.textContent = order1.pancakes;
+pancakes1.textContent = order1.dataset.pancakes;
 status1.textContent = order1.dataset.delivered === 'true' ? 'Delivered' : 'Pending';
 
 biscuits2.textContent= order2.dataset.biscuits;
 donuts2.textContent = order2.dataset.donuts;
-pancakes2.textContent = order2.pancakes;
+pancakes2.textContent = order2.dataset.pancakes;
 status2.textContent = order2.dataset.delivered === 'true' ? 'Delivered' : 'Pending';
 
 biscuits3.textContent= order3.dataset.biscuits;
 donuts3.textContent = order3.dataset.donuts;
-pancakes3.textContent = order3.pancakes;
+pancakes3.textContent = order3.dataset.pancakes;
 status3.textContent = order3.dataset.delivered === 'true' ? 'Delivered' : 'Pending';
