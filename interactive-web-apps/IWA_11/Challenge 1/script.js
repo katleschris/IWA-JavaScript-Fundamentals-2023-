813df1dd56b13528f296de9d25f1e8894da5e56e
Katlego Christabel Leshiba <106029638+katleschris@users.noreply.github.com>
2023-04-17 11:44:21 +0200

    Fixed variable declaration

--- interactive-web-apps/IWA_11/Challenge 1/script.js
+++ interactive-web-apps/IWA_11/Challenge 1/script.js
@@ -1,32 +1,35 @@
-const 1-root = document(order1),
-const 1-biscuits: document(biscuits),
-const 1-donuts: document(donuts),
-const 1-pancakes: document(pancakes),
-const 1-status: document(status)
+// for order 1
+const order1= document(order1),
+const biscuits1= document(biscuits1),
+const donuts1= document(donuts1),
+const pancakes1= document(pancakes1),
+const status1= document(status1)
 
-const 2-root = document(order2),
-const 2-biscuits: document(biscuits),
-const 2-donuts: document(donuts),
-const 2-pancakes: document(pancakes),
-const 2-status: document(status)
+//for order 2
+const order2 = document(order2),
+const biscuits2= document(biscuits2),
+const donuts2 = document(donuts2),
+const pancakes2 = document(pancakes2),
+const status2 = document(status2)
 
-const 3-root = document(order3),
-const 3-biscuits: document(biscuits),
-const 3-donuts: document(donuts),
-const 3-pancakes: document(pancakes),
-const 3-status: document(status)
+//For order 3
+const order3 = document(order3),
+const biscuits3 = document(biscuits3),
+const donuts3 = document(donuts3),
+const pancakes3 = document(pancakes3),
+const status3 = document(status3)
 
-1-biscuits= 1-root.biscuits,
-1-donuts = 1-root.donuts,
-1-pancakes = 1-root.pancakes,
-1-status = 1-root.status ? Delivered : Pending
+biscuits1= root.biscuits1,
+donuts1 = root.donuts1,
+pancakes1 = root.pancakes1,
+status1 = root.status1 ? Delivered : Pending
 
-2-biscuits= 2-root.biscuits,
-2-donuts = 2-root.donuts,
-2-pancakes = 2-root.pancakes,
-2-status = 2-root.status ? Delivered : Pending
+biscuits2= root.biscuits2,
+donuts2 = root.donuts2,
+pancakes2 = root.pancakes2,
+status2 = root.status2 ? Delivered : Pending
 
-3-biscuits= 3-root.biscuits,
-3-donuts = 3-root.donuts,
-3-pancakes = 3-root.pancakes,
-3-status = 3-root.status ? Delivered : Pending
+biscuits3= root.biscuits3,
+donuts3 = root.donuts3,
+pancakes3 = root.pancakes3,
+status3 = root.status3 ? Delivered : Pending
