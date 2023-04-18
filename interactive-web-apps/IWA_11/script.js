5b6a3c813894c13b01954ff8bd195fdb01cdae40
Katlego Christabel Leshiba <106029638+katleschris@users.noreply.github.com>
2023-04-18 11:35:49 +0200

    Corrected CSS citation

--- interactive-web-apps/IWA_11/script.js
+++ interactive-web-apps/IWA_11/script.js
@@ -1,22 +1,22 @@
 // for order 1
 const order1= document.querySelector('[data-key = "order1"]');
-const biscuits1= order1.querySelector('.biscuits.count');
-const donuts1= order1.querySelector('.donuts.count');
-const pancakes1= order1.querySelector('.pancakes.count');
+const biscuits1= order1.querySelector('.biscuits .count');
+const donuts1= order1.querySelector('.donuts .count');
+const pancakes1= order1.querySelector('.pancakes .count');
 const status1= order1.querySelector('.status dd');
 
 //for order 2
 const order2= document.querySelector('[data-key = "order2"]');
-const biscuits2= order2.querySelector('.biscuits.count');
-const donuts2= order2.querySelector('.donuts.count');
-const pancakes2= order2.querySelector('.pancakes.count');
+const biscuits2= order2.querySelector('.biscuits .count');
+const donuts2= order2.querySelector('.donuts .count');
+const pancakes2= order2.querySelector('.pancakes .count');
 const status2= order2.querySelector('.status dd');
 
 //For order 3
 const order3= document.querySelector('[data-key = "order3"]');
-const biscuits3= order3.querySelector('.biscuits.count');
-const donuts3= order3.querySelector('.donuts.count');
-const pancakes3= order3.querySelector('.pancakes.count');
+const biscuits3= order3.querySelector('.biscuits .count');
+const donuts3= order3.querySelector('.donuts .count');
+const pancakes3= order3.querySelector('.pancakes .count');
 const status3= order3.querySelector('.status dd');
 
 // replace the loading prompts with the data from the database
---
+++ interactive-web-apps/IWA_12/script.js
@@ -0,0 +1,64 @@
+const STATUS_MAP = {
+    shelf: {
+        color: 'green',
+        canReserve: true,
+        canCheckout: true,
+        canCheckIn: false,
+    },
+    reserved: {
+        color: 'blue',
+        canReserve: false,
+        canCheckout: true,
+        canCheckIn: false,
+    },
+    overdue: {
+        color: 'red',
+        canReserve: false,
+        canCheckout: false,
+        canCheckIn: true,
+    },
+    checkedOut: {
+        color: 'orange',
+        canReserve: false,
+        canCheckout: false,
+        canCheckIn: true,
+    }
+}
+
+// Edit below line 
+
+//book1
+const status = document.getElementsByClassName('status')
+const reserve = document.getElementsByClassName(reserve)
+const checkout = document.getElementsByClassName(checkout)
+//const checkin = document.getElementsByClassName(checkin)
+console.log(status.textContent)
+//book2
+status = selector(status)
+reserve = selector(reserve)
+checkout = selector(checkout)
+checkin = selector(checkin)
+
+//book3
+status = selector(status)
+reserve = selector(reserve)
+checkout = selector(checkout)
+checkin = selector(checkin)
+
+checkin.0.color = none
+status.0.style.color = STATUS_MAP.status.color
+reserve.0 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
+checkout.0 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
+checkin.0 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+
+checkin.1.color = none
+status.1.style.color = STATUS_MAP.status.color
+reserve.1 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
+checkout.1 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
+checkin.1 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+
+checkin.2.color = none
+status.2.style.color = STATUS_MAP.status.color
+reserve.2 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
+checkout.2 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
+checkin.2 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
